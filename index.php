352d4e14a0091624f93547068ffc47a0cbc3477e
Cássio Tieres Bomfim <tieresbomfim@gmail.com>
2022-01-21 16:21:44 -0300

    Está dando um fatal error, verificar mais tarde

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
-<!DOCTYPE html>
-    <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Gerando PDF com PHP</title>
-    </head>
-    <body>
-        
-    </body>
-    </html>
+<?php
+
+use Dompdf\Dompdf;
+
+require __DIR__.'/vendor/autoload.php';
+
+     $dpdf = new Dompdf();
+
+     $tabela = file_get_contents('tabela.html');
+
+     $dpdf->loadhtml($tabela);
+     $dpdf->setPaper('A4', 'portrait');
+     $dpdf->render();
+     $dpdf->stream();
+?> 
